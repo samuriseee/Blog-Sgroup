0da1fe183c2e1994f955ead01529655351e55d70
linh <hoanglinhphan16@gmail.com>
2021-07-25 16:44:26 +0700

    Signup/Login(not responsive)

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import firebase from "firebase/app";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
-Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+const firebaseConfig = {
+  apiKey: "AIzaSyCmEsx1bAezxDU639Fd5pqfNlGeckmLkJY",
+  authDomain: "blog-sgroup.firebaseapp.com",
+  projectId: "blog-sgroup",
+  storageBucket: "blog-sgroup.appspot.com",
+  messagingSenderId: "329854933967",
+  appId: "1:329854933967:web:0f9df333bd526abed890ba",
+  measurementId: "G-DXMNG8J33C",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,54 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/login",
+    name: "Login",
+    component: Login,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: { requireAuth: true },
+  },
+  {
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    meta: { requireAuth: true },
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const requireAuth = to.matched.some((record) => record.meta.requireAuth);
+  const isAuth = firebase.auth().currentUser;
+  if (requireAuth && !isAuth) {
+    next("/login");
+  } else next();
+});
+
+export default router;
