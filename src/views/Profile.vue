8a9833ec82acf80bc1e80b2f0451dd410f7d0521
samuriseee <gonhungho123@gmail.com>
2021-09-13 16:18:35 +0700

    profile styling

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -5,13 +5,15 @@
       <h2>Account Settings</h2>
       <div class="profile-info">
         <div class="initials">{{ $store.state.profileInitials }}</div>
-        <div class="input">
-          <label for="firstName">First Name:</label>
-          <input type="text" id="firstName" v-model="firstName" />
-        </div>
-        <div class="input">
-          <label for="lastName">Last Name:</label>
-          <input type="text" id="lastName" v-model="lastName" />
+        <div class="flexName"> 
+          <div class="input">
+            <label for="firstName">First Name:</label>
+            <input type="text" id="firstName" v-model="firstName" />
+          </div>
+          <div class="input">
+            <label for="lastName">Last Name:</label>
+            <input type="text" id="lastName" v-model="lastName" />
+          </div>
         </div>
         <div class="input">
           <label for="username">Username:</label>
@@ -84,6 +86,11 @@ export default {
 .profile {
   .container {
     max-width: 1000px;
+    height:100vh;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     padding: 60px 25px;
     h2 {
       text-align: center;
@@ -95,7 +102,7 @@ export default {
       border-radius: 8px;
       box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
       padding: 32px;
-      background-color: #f1f1f1;
+      background-color: #000000;
       display: flex;
       flex-direction: column;
       max-width: 600px;
@@ -105,44 +112,36 @@ export default {
         width: 80px;
         height: 80px;
         font-size: 32px;
-        background-color: #303030;
-        color: #fff;
+        background-color: rgb(255, 255, 255);
+        color: rgb(0, 0, 0);
         display: flex;
         align-self: center;
         align-items: center;
         justify-content: center;
         border-radius: 50%;
       }
-      .admin-badge {
-        display: flex;
-        align-self: center;
-        color: #fff;
-        font-size: 14px;
-        padding: 8px 24px;
-        border-radius: 8px;
-        background-color: #303030;
-        margin: 16px 0;
-        text-align: center;
-        text-transform: capitalize;
-        .icon {
-          width: 14px;
-          height: auto;
-          margin-right: 8px;
-        }
-      }
       .input {
         margin: 16px 0;
+        
         label {
           font-size: 14px;
           display: block;
           padding-bottom: 6px;
+          color:#fff;
+          text-transform: uppercase;
         }
         input {
           width: 100%;
           border: none;
-          background-color: #f2f7f6;
+          border: none;
+          border-bottom: 2px solid rgb(255, 255, 255);
+          outline: none;
+          background: transparent;
+          color: #ffffff;
+          font-size: 16px;
           padding: 8px;
           height: 50px;
+          text-align: center;
           @media (min-width: 900px) {
           }
           &:focus {
@@ -151,9 +150,26 @@ export default {
         }
       }
       button {
+        display: flex;
         align-self: center;
+        align-items: center;
+        border: none;
+        outline: none;
+        height: 40px;
+        background: rgb(255, 255, 255);
+        color:rgb(0, 0, 0);
+        font-size: 18px;
+        border-radius: 20px;
+        padding: 15px;
       }
     }
+  }
+}
+.flexName {
+  display:flex;
+  justify-content: space-between;
+  .input {
+    padding: 20px;
   }
 }
 </style>
